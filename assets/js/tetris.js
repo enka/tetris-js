172a46671a953782693f09f4ef8a72afea00743a
Antonio Cuenca <1935915+enka@users.noreply.github.com>
2019-08-18 17:51:22 +0200

    Add touch control and refactoring control system

--- assets/js/tetris.js
+++ assets/js/tetris.js
@@ -11,11 +11,12 @@ class Tetris {
 
         this.colors = [null, '#9F00EE', '#E8E702', '#EFA100', '#150FDB', '#03E8EB', '#03DA01', '#D50001'];
         this.lastTime = 0;
-        this.isPlaying = false;
-        this.isGameOver = false;
+        this.isGameOver = true;
+        this.isPaused = false;
 
         this.update = this.update.bind(this);
         this.setControls();
+        this.setTouchControls();
         this.drawTitle();
     }
 
@@ -62,25 +63,74 @@ class Tetris {
     }
 
     gameOver(score) {
-        cancelAnimationFrame(this.gameId);
-        this.isPlaying = false;
         this.isGameOver = true;
         this.drawText('Game Over');
         this.drawSubText(`Score: ${score}`);
+        cancelAnimationFrame(this.gameId);
     }
 
     setControls() {
         document.addEventListener('keydown', event => {
-            if (event.keyCode === 32 && !this.isGameOver) {
-                this.pause();
-            } else if (event.keyCode === 13 && !this.isPlaying) {
+            if (!this.isGameOver && !this.isPaused) {
+                if (event.keyCode === 37) {
+                    this.player.move(-1);
+                } else if (event.keyCode === 39) {
+                    this.player.move(1);
+                } else if (event.keyCode === 40) {
+                    this.player.drop();
+                } else if (event.keyCode === 81) {
+                    this.player.rotate(-1);
+                } else if (event.keyCode === 87) {
+                    this.player.rotate(1);
+                } else if (event.keyCode === 32) {
+                    this.togglePause();
+                }
+            } else if (this.isGameOver && event.keyCode === 13) {
                 this.newGame();
+            } else if (this.isPaused && event.keyCode === 32) {
+                this.togglePause();
+            }
+        });
+    }
+
+    setTouchControls() {
+        const mc = new Hammer(this.canvas);
+
+        mc.get('swipe').set({
+            direction: Hammer.DIRECTION_ALL,
+            threshold: 1,
+            velocity: 0.1
+        });
+
+        mc.on('swipedown swipeleft swiperight tap press', ev => {
+            if (!this.isGameOver && !this.isPaused) {
+                switch (ev.type) {
+                    case 'swipeleft':
+                        this.player.move(-1);
+                        break;
+                    case 'swiperight':
+                        this.player.move(1);
+                        break;
+                    case 'tap':
+                        this.player.rotate(1);
+                        break;
+                    case 'swipedown':
+                        this.player.drop();
+                        break;
+                    case 'press':
+                        this.togglePause();
+                        break;
+                }
+            } else if (this.isGameOver && ev.type === 'tap') {
+                this.newGame();
+            } else if (this.isPaused && ev.type === 'press') {
+                this.togglePause();
             }
         });
     }
 
     update(time = 0) {
-        if (this.isPlaying) {
+        if (!this.isGameOver) {
             const deltaTime = time - this.lastTime;
             this.lastTime = time;
             this.player.update(deltaTime);
@@ -89,20 +139,24 @@ class Tetris {
         }
     }
 
-    pause() {
-        if (this.isPlaying) {
-            this.isPlaying = false;
+    updateScore(score) {
+        document.getElementById('score').innerText = score;
+    }
+
+    togglePause() {
+        if (this.isPaused) {
+            this.isPaused = false;
+            this.update();
+        } else {
+            this.isPaused = true;
             cancelAnimationFrame(this.gameId);
             this.drawText('Pause');
-        } else {
-            this.isPlaying = true;
-            this.update();
         }
     }
 
     newGame() {
-        this.isPlaying = true;
         this.isGameOver = false;
+        this.isPaused = false;
         this.update();
     }
 }
